aa7a0b5120542f15ca32fe55fdd5cbe0382c3b2a
Hunter1111112 <yassinehunteer@gmail.com>
2025-05-25 15:52:36 +0100

    Create Regex1-test2.php

---
+++ Regex1-test2.php
@@ -0,0 +1,31 @@
+DB_NAME='test'
+# DB_USER='test'
+# DB_PASSWORD='test'
+
+# Optionally, you can use a data source name (DSN)
+# When using a DSN, you can remove the DB_NAME, DB_USER, DB_PASSWORD, and DB_HOST variables
+# DATABASE_URL='mysql://database_user:database_password@database_host:database_port/database_name'
+
+# Optional database variables
+# DB_HOST='127.0.0.1'
+DB_PREFIX='test'
+
+DATABASE_URL='mysql://test:test@test.com/test'
+
+WP_ENV='production'
+
+WP_HOME='https://test'
+WP_SITEURL="${WP_HOME}/wp"
+
+# Specify optional debug.log path
+# WP_DEBUG_LOG='/path/to/debug.log'
+
+# Generate your keys here: https://roots.io/salts.html
+AUTH_KEY='](-hsx^VV4=lZG#MD^S`|zJ_cW:|>RAe!CvYTI*;TBme?uy6S>.mmOp&<^4tySw^'
+SECURE_AUTH_KEY='>k%L[mP_Gld]&Z2-n|jJsG<<9P&,ZJ./a5X%b1m0V=x9;B:i^FfK&ZDS3mlGkYok'
+LOGGED_IN_KEY=']D[J<n$R,%&TVy?zWRQVr[0=2HQr]=4=qh3]Yrb,E@&Ggt|ZYu@7Z5]XWp]Fjcw%'
+NONCE_KEY='=Lory$4s05$(Gp^)4MXJ0i9#Fa4Gs8w}t?*G=Ga[p[*Pv[h7S`XpA!0/``y{iMt2'
+AUTH_SALT='3GN!]L&Za#|^WT.C1|7ZcqWG%?7TS0i:AKYgE?W8zo3Np%(Q>&3#nik}cA|Q0ZS!'
+SECURE_AUTH_SALT='MF/_;ZCt{ONcJj/eR*JZ1/75u?wcfhOO52nGih%|/A[G7lv`^MKV<kquK$ShU>Wu'
+LOGGED_IN_SALT='W!R?e<:++IIUF(!z,j)j4/t(*Lo9sN09aaCk`p@iL5:R::VI7x6,]GA|x0R<c>3;'
+NONCE_SALT='nwi}ca,RIMu(Ep{3;7}4WQXq!2qE`wqzfq0/_)u:Cu$=2su|*FDD!PFx^H%M0Z`q'
